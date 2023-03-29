a534279076ffb42082416210a96ed693e8ec2034
daveweilert <daveweilert@gmail.com>
2023-03-29 15:54:28 -0500

    cmdI chg

--- getCmdInfo.py
+++ getCmdInfo.py
@@ -226,7 +226,6 @@ def getMemory():
 
         cnt = 0
         for m in cda.cpu_ram:
-            print(m)
             if int(m[1]) > u_high:
                 u_high = int(m[1])
             if int(m[1]) < u_low:
